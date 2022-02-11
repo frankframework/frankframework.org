ca1db460ebc7ab950ec70e4731d541078de507d7
Niels Lam <hoi@niels.fun>
2022-02-12 00:02:58 +0100

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    devServer: {
+        static: {
+          directory: path.join(__dirname, 'public'),
+        },
+        compress: true,
+        port: 9000,
+    },
+    mode: "production",
+    entry: './src/js/index.js',
+    output: {
+        filename: 'main.[contenthash].js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                { from: "./src/assets", to: "assets" },
+            ],
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/index.html',
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].[contenthash].css",
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    {
+                        loader: "sass-loader",
+                        options: {
+                          implementation: require.resolve("sass"),
+                        },
+                    },
+                ]
+            }
+        ]
+    }
+};
