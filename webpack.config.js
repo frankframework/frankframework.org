255a1f7c3602b60d6416c7ff41e5e9d417d86bc1
Niels Lam <hoi@niels.fun>
2022-02-17 18:42:09 +0100

    Add base tag to build in production mode

--- webpack.config.js
+++ webpack.config.js
@@ -3,51 +3,57 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-    devServer: {
-        static: {
-          directory: path.join(__dirname, 'public'),
+const TARGET_URL = "https://nielslam.github.io/frankframework.org";
+
+module.exports = (env, argv) => {
+    return {
+        devServer: {
+            static: {
+            directory: path.join(__dirname, 'public'),
+            },
+            compress: true,
+            port: 9000,
         },
-        compress: true,
-        port: 9000,
-    },
-    mode: "production",
-    entry: './src/js/index.js',
-    output: {
-        filename: 'main.[contenthash].js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true
-    },
-    plugins: [
-        new CopyPlugin({
-            patterns: [
-                { from: "./src/assets", to: "assets" },
-            ],
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            template: './src/index.html',
-            inject: 'body'
-        }),
-        new MiniCssExtractPlugin({
-            filename: "[name].[contenthash].css",
-        })
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    "css-loader",
-                    {
-                        loader: "sass-loader",
-                        options: {
-                          implementation: require.resolve("sass"),
+        mode: 'development',
+        entry: './src/js/index.js',
+        output: {
+            filename: 'main.[contenthash].js',
+            path: path.resolve(__dirname, 'dist'),
+            
+            clean: true
+        },
+        plugins: [
+            new CopyPlugin({
+                patterns: [
+                    { from: "./src/assets", to: "assets" },
+                ],
+            }),
+            new HtmlWebpackPlugin({
+                filename: 'index.html',
+                template: './src/index.html',
+                inject: 'body',
+                base: argv.mode === 'production' ? TARGET_URL : '/',
+            }),
+            new MiniCssExtractPlugin({
+                filename: "[name].[contenthash].css",
+            })
+        ],
+        module: {
+            rules: [
+                {
+                    test: /\.scss$/,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        "css-loader",
+                        {
+                            loader: "sass-loader",
+                            options: {
+                            implementation: require.resolve("sass"),
+                            },
                         },
-                    },
-                ]
-            }
-        ]
+                    ]
+                }
+            ]
+        }
     }
 };
